a3d34acde573d4e4536f44a771a4fcd8e9da6065
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-10-15 23:26:19 -0500

    Initial commit

---
+++ TrabajoArquitectura/src/main/java/arquitectura/TrabajoArquitectura/TrabajoArquitecturaApplication.java
@@ -0,0 +1,13 @@
+package arquitectura.TrabajoArquitectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoArquitecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoArquitecturaApplication.class, args);
+	}
+
+}
---
+++ TrabajoArquitectura/src/test/java/arquitectura/TrabajoArquitectura/TrabajoArquitecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitectura.TrabajoArquitectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoArquitecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
