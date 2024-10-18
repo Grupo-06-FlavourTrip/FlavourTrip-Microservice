0d154fdaf81af0682123a829f5274192477979f8
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-10-18 00:05:07 -0500

    microservices base

---
+++ TrabajoArquitectura/msvc-tour/src/main/java/com/flavourtrip/msvc_tour/MsvcTourApplication.java
@@ -0,0 +1,13 @@
+package com.flavourtrip.msvc_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcTourApplication.class, args);
+	}
+
+}
